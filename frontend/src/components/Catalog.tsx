276f037d45f4adf1885cbaa92ffeaa3b65e32672
Crhistopher Gutierrez <crhistopherisai@gmail.com>
2025-04-25 22:47:56 -0600

    update catalog

--- frontend/src/components/Catalog.tsx
+++ frontend/src/components/Catalog.tsx
@@ -14,7 +14,7 @@ export default function Catalog() {
           ¿Qué contiene nuestras mermeladas?
         </h2>
         <div className="flex flex-col gap-6 md:flex-row md:gap-10">
-          <div className="bg-rose-400 rounded-3xl p-6 text-white w-full max-w-sm flex flex-col items-center text-center">
+          <div className="bg-orange-400 rounded-3xl p-6 text-white w-full max-w-sm flex flex-col items-center text-center">
             <img src="/mermelarte-papaya.png" alt="Papaya" className="w-20 h-20 mb-4" />
             <h4 className="text-lg font-bold mb-2">PAPAYA</h4>
             <p className="mb-4">
@@ -24,7 +24,7 @@ export default function Catalog() {
             Mas informacion &gt;
             </button>
           </div>
-          <div className="bg-rose-400 rounded-3xl p-6 text-white w-full max-w-sm flex flex-col items-center text-center">
+          <div className="bg-amber-900 rounded-3xl p-6 text-white w-full max-w-sm flex flex-col items-center text-center">
             <img src="/mermelarte-tamarindo.png" alt="Tamarindo y Chipotle" className="w-20 h-20 mb-4" />
             <h4 className="text-lg font-bold mb-2">TAMARINDO Y CHIPOTLE</h4>
             <p className="mb-4">
@@ -37,19 +37,51 @@ export default function Catalog() {
         </div>
       </section>
 
-      {/* Información adicional */}
-      <section id="papaya-info" className="w-full px-6 py-8 bg-white text-center">
-        <h3 className="text-xl font-bold text-amber-800 mb-2">Materiales - Papaya</h3>
-        <p>
-          Papaya fresca, azúcar orgánica, jugo de limón, pectina natural. Todos los ingredientes son seleccionados cuidadosamente para mantener sus propiedades.
-        </p>
+      <section className="w-full bg-white text-center p-6">
+        <h1 className=" text-3xl p-2 bg-gray-200 rounded-full uppercase">Ingredientes</h1>
       </section>
 
-      <section id="tamarindo-info" className="w-full px-6 py-8 bg-white text-center">
-        <h3 className="text-xl font-bold text-amber-800 mb-2">Materiales - Tamarindo y Chipotle</h3>
+      {/* Información adicional */}
+      <section id="papaya-info" className="w-full px-6 py-8 bg-white text-white text-center">
+        <div className="bg-orange-400 rounded-4xl p-6">
+        <h3 className="text-xl font-bold mb-2">Papaya</h3>
+        <ul className=" list-inside list-disc text-justify">
+          <li>Papaya fresca 650gr</li>
+          <li>Azucar orgánica 350gr</li>
+          <li>Jugo de limón 1gr</li>
+          <li>Benzoato de sodio 1gr</li>
+          <li>Agua purificada 200ml</li>
+        </ul>
+        </div>
+      </section>
+      <section id="papaya-info" className="w-full px-6 bg-white text-white text-center">
+        <div className="bg-amber-900 rounded-4xl p-6 mb-6">
+        <h3 className="text-xl font-bold mb-2">Tamarindo y Chipotle</h3>
+        <ul className=" list-inside list-disc text-justify">
+          <li>Tamarindo chipotle</li>
+          <li>Azucar orgánica 350gr</li>
+          <li>Jugo de limón 1gr</li>
+          <li>Vinagre blanco 230ml</li>
+          <li>Benzoato de sodio 1gr</li>
+          <li>Agua purificada 200ml</li>
+        </ul>
+        </div>
+      </section>
+      <section className="w-full px-6 py-8 text-center bg-amber-100">
+        <h2 className="text-2xl">Necesidades</h2>
         <p>
-          Tamarindo maduro, chipotle seco, panela, sal marina y un toque de especias. Un balance perfecto entre dulce, ácido y picante.
+        Salud y Nutrición: Producto natural, sin conservantes ni aditivos, con alto contenido de fruta, fibra y antioxidantes.
         </p>
+        <p>
+        Variedad y Sabor: Sabores únicos con frutas locales como mamey, pitahaya y nance, ofreciendo una alternativa diferente a las mermeladas comerciales.
+        </p>
+        <p>
+        Economía Local: Apoya a productores regionales, genera empleo y fomenta el comercio justo.
+        </p>
+        <p>
+        Sostenibilidad: Uso de envases reutilizables, menor huella de carbono y aprovechamiento de excedentes agrícolas.
+        </p>
+
       </section>
 
       {/* Footer */}
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -12,6 +12,18 @@ export default function Home() {
           <img src="/mermelarte-logo.png" alt="Mermelarte logo" className="h-25 w-25"/>
         </section>
         
+        <section className="flex flex-col text-justify items-center italic p-6 bg-gray-100 mx-4 border-rose-100 drop-shadow-md rounded-3xl my-5">
+          <p>
+          Las mermeladas son productos obtenidos a partir de frutas y azúcares, sometidos a un proceso de concentración y gelificación mediante pectinas naturales. 
+          </p>
+          <p>
+            La adición de dulces tradicionales yucatecos puede modificar su textura, sabor y estabilidad, lo que requiere un análisis detallado de los componentes involucrados, como azúcares, ácidos orgánicos y compuestos fenólicos.
+          </p>
+          <p>
+          Este proyecto no solo responde a la demanda de productos más naturales y funcionales, sino que también impulsa el rescate de frutos autóctonos, reduce el impacto ambiental.
+          </p>
+        </section>
+
         <section className="flex flex-col items-center text-center p-6 bg-amber-200 mx-4 border-rose-100 drop-shadow-md rounded-3xl my-5">
           <h2 className="text-3xl font-bold text-amber-900 mb-4">
             Conoce nuestras deliciosas mermeladas artesanales
@@ -20,6 +32,7 @@ export default function Home() {
             EXPLORAR AHORA
           </Link>
         </section>
+        
         
         <section className="w-full flex justify-center mb-6">
           <img
